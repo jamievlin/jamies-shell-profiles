122e1f6ab84f3066d6ab67488ae889a17098ecf3
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-12-20 14:48:06 -0700

    PWSH: Add powershell profile.

---
+++ pwsh-profile.ps1
@@ -0,0 +1,57 @@
+#!/usr/bin/env pwsh
+
+Import-Module posh-git
+
+function isadmin {
+    ([Security.Principal.WindowsPrincipal] `
+            [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+            [Security.Principal.WindowsBuiltInRole] "Administrator")
+}
+
+if (isadmin) {
+    $GitPromptSettings.DefaultPromptPrefix.Text = '[Admin] '
+    $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Red
+}
+
+$GitPromptSettings.DefaultPromptPath.ForegroundColor = [ConsoleColor]::Green
+$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+
+function prompt {
+    & $GitPromptScriptBlock
+}
+
+function admin {
+    param (
+    [Parameter(Mandatory=$false)]
+    $prog = "pwsh"
+    )
+    start $prog -Verb RunAs
+}
+
+function Vs-DevEnv {
+    Write-Error "Visual Studio installation not found"
+}
+
+if (Get-Command Get-CimInstance -errorAction SilentlyContinue)
+{
+    $VsInfo = Get-CimInstance MSFT_VSInstance -Namespace root/cimv2/vs
+
+    if ($VsInfo -eq $null) {
+        return
+    }
+
+    function Vs-DevEnv {
+        & "$($VsInfo.InstallLocation)\\Common7\\Tools\\Launch-VsDevShell.ps1" -Arch amd64 -HostArch amd64 -SkipAutomaticLocation
+    }
+}
+
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+Set-PSReadLineOption -PredictionSource Plugin
+
+if (Get-Command gpg-connect-agent -errorAction SilentlyContinue) {
+    Invoke-Expression "gpg-connect-agent reloadagent /bye" > $null
+}
+
+if (Test-Path -Path "$env:VCPKG_ROOT") {
+    Import-Module "${env:VCPKG_ROOT}\scripts\posh-vcpkg"
+}
