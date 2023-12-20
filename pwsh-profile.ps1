51ddc58a9e5a938f82cf0ce9e8dcb8c5d1dbbebb
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-12-20 15:09:30 -0700

    PWSH: Add WarningAction to Get-CimInstance.

--- pwsh-profile.ps1
+++ pwsh-profile.ps1
@@ -35,7 +35,7 @@ function Vs-DevEnv {
 
 if (Get-Command Get-CimInstance -errorAction SilentlyContinue)
 {
-    $VsInfo = Get-CimInstance MSFT_VSInstance -Namespace root/cimv2/vs
+    $VsInfo = Get-CimInstance MSFT_VSInstance -Namespace root/cimv2/vs -WarningAction:SilentlyContinue
 
     if ($VsInfo -ne $null) {
         function Vs-DevEnv {
